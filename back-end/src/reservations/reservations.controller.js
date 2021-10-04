bb6db6478ed19d240bf72cf0859fdec52873cb8c
MJSBDLK <rqdavismail@gmail.com>
2021-10-04 13:03:18 -0700

    US-02 and US-03 tests passing.

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -4,8 +4,46 @@ const service = require('./reservations.service');
 /**
  * List handler for reservation resources
  */
+function formatDate(date, time) {
+  // console.log(`date, time: `, date, time);
+  const result = new Date();
+  result.setFullYear(date.slice(0,4));
+  result.setMonth(date.slice(5,7));
+  result.setDate(date.slice(8));
+  result.setHours(parseInt(time.slice(0,2)),parseInt(time.slice(3,5)),0);
+  // console.log(`result, step 3: `, result);
+  return result;
+}
+
+function isTuesday(date) {
+  const resDate = new Date (date.split('-').join('/'))
+  const day = resDate.getDay();
+  if (day === 2) return true;
+  return false;
+}
+
+function isPast(date, time) {
+  const resDate = formatDate(date, time);
+  const rightNow = new Date();
+  // console.log(`resDate, rightNow: `, resDate, rightNow);
+  if (resDate < rightNow) return true;
+  return false;
+}
+
+function restaurantOpen(time) {
+  const resTime = new Date();
+  resTime.setHours(parseInt(time.slice(0,2)),parseInt(time.slice(3,5)),0);//Jank-ass workaround
+  const open = new Date();
+  const close = new Date();
+  open.setHours(10,30,0);
+  close.setHours(21,30,0);
+  if (resTime > open && resTime < close) return true;
+  return false;
+}
+
 function validateReservation(req, res, next) {
   const newReservation = req.body.data;
+  // console.log(`newReservation: `, newReservation);
   if (!newReservation) return next({
     status: 400,
     message: `no data submitted`
@@ -19,9 +57,12 @@ function validateReservation(req, res, next) {
       errors.push(`${field} field is required.`)
     }
   });
-  if (typeof newReservation.people !== 'number') errors.push(`people field must be a number`);
-  if (!/\d\d:\d\d/.test(newReservation.reservation_time)) errors.push(`reservation_time must have format HH:MM`);
-  if (Number.isNaN(Date.parse(newReservation.reservation_date))) errors.push(`reservation_date must have format YYYY:MM:DD`);
+  if (typeof newReservation.people !== 'number') errors.push(`people field must be a number.`); // tests test for lowercase "people" for some reason
+  if (!/\d\d:\d\d/.test(newReservation.reservation_time)) errors.push(`reservation_time must have format HH:MM.`); //regex wizardry courtesy of Justin
+  if (Number.isNaN(Date.parse(newReservation.reservation_date))) errors.push(`reservation_date must have format YYYY:MM:DD.`);
+  if (isTuesday(newReservation.reservation_date)) errors.push(`The restaurant is closed on Tuesdays.`);
+  if (isPast(newReservation.reservation_date, newReservation.reservation_time)) errors.push(`No time travelers: reservation date and time must be in the future.`);
+  if (!restaurantOpen(newReservation.reservation_time)) errors.push(`Reservation must be between 10:30 AM and 9:30 PM.`);
   // console.log(`errors: `, errors)
   if (errors.length) {
     return next({
